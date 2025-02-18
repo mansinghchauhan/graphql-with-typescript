fec4a855617211c58f096ba0e9b8fcd473ae91f5
man singh <man.singh@vegavid.com>
2025-02-18 22:37:23 +0530

    removed node module

--- dist/models/userModel.js
+++
@@ -1,6 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const userSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true }
-}, { timestamps: true });
-export default mongoose.model("User", userSchema);
